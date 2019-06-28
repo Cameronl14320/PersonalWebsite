369fcea1f4caa3511cf0e13efbbffab5bd081ba8
Mustemploy <41677875+Mustemploy@users.noreply.github.com>
2019-06-29 00:35:11 +1200

    Biography add

--- docs/main/script.js
+++ docs/main/script.js
@@ -1,8 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-/* Set the width of the side navigation to 250px */
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-/* Set the width of the side navigation to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
 }
