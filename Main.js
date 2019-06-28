732c7dadf787d524698b2ba86d3cbbe885e41d3b
Mustemploy <41677875+Mustemploy@users.noreply.github.com>
2019-06-28 23:56:55 +1200

    Upload From Pen

---
+++ Main.js
@@ -0,0 +1,14 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+/* Set the width of the side navigation to 250px */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  } 
+  
