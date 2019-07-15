155ca87b595acb3e7088f7663c634cd300eef40f
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2019-07-16 11:22:45 +1200

    experimenting

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,13 @@
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.height = "0px";
-        document.getElementById("main").style.paddingTop = "0px";
+    if (document.body.scrollTop > 25 || document.documentElement.scrollTop > 25) {
+        document.getElementById("header").style.height = "25px";
+        document.getElementById("main").style.paddingTop = "30px";
+        document.getElementById("reveal").style.height = "25px";
     } else {
-        document.getElementById("header").style.height = "125px";
-        document.getElementById("main").style.paddingTop = "130px";
+        document.getElementById("header").style.height = "65px";
+        document.getElementById("main").style.paddingTop = "80px";
+        document.getElementById("reveal").style.height = "0px";
     }
 }
