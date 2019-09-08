067008f64a65d71c500e605f1580eaa9bac842ad
Cameronl14320 <41677875+Cameronl14320@users.noreply.github.com>
2019-09-08 23:00:17 +1200

    style changes, js experiment, layout and content

---
+++ docs/landing/script.js
@@ -0,0 +1,6 @@
+
+function reveal() {
+    console.log("works");
+    document.getElementById("reveal-info").style.width=("0px");
+    document.getElementById("reveal-info").style.height=("0px");
+}
