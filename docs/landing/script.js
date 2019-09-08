461f4f2d64886be183181d06704e2ba58099b597
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2019-09-09 11:36:35 +1200

    Timeout functions, button and text hide transition

--- docs/landing/script.js
+++ docs/landing/script.js
@@ -1,6 +1,22 @@
+function hideReveal() {
+    console.log("works hideReveal");
+    document.getElementById("reveal-info").style.opacity = "0";
+    document.getElementById("reveal-info").style.width = "0";
+    document.getElementById("reveal-info").style.height = "0";
+    // document.getElementById("reveal-info").style.display = "none";
+    setTimeout(hideMainBodyText, 125);
+}
 
-function reveal() {
-    console.log("works");
-    document.getElementById("reveal-info").style.width=("0px");
-    document.getElementById("reveal-info").style.height=("0px");
+function hideMainBodyText() {
+    console.log("works hideMainBodyText");
+    document.getElementById("main-reveal-h1").style.opacity = "0";
+    document.getElementById("main-reveal-h1").style.fontSize = "0";
+    setTimeout(revealInfo, 250);
+    // document.getElementById("main-reveal-h1").style.width = "0";
+    // document.getElementById("main-reveal-h1").style.height = "0";
+}
+
+// Recursive Method
+function revealInfo() {
+
 }
