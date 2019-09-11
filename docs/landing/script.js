c78e395193d45ee502850bedb17bb9518cfd642e
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2019-09-11 14:31:54 +1200

    saved changes

--- docs/landing/script.js
+++ docs/landing/script.js
@@ -1,24 +1,37 @@
+var hidden = false;
+
 function hideReveal() {
     console.log("works hideReveal");
-    document.getElementById("reveal-info").style.opacity = "0";
-    document.getElementById("reveal-info").style.width = "0";
-    document.getElementById("reveal-info").style.height = "0";
-    // document.getElementById("reveal-info").style.display = "none";
-    setTimeout(hideMainBodyText, 125);
+    if (hidden == false) {
+        document.getElementById("reveal-info").style.opacity = "0";
+        document.getElementById("reveal-info").style.width = "0";
+        document.getElementById("reveal-info").style.height = "0";
+        // document.getElementById("reveal-info").style.display = "none";
+        setTimeout(hideMainBodyText, 125);
+    }
 }
 
 function hideMainBodyText() {
     console.log("works hideMainBodyText");
-    document.getElementById("main-reveal-h1").style.opacity = "0";
-    document.getElementById("main-reveal-h1").style.fontSize = "0";
-    document.getElementById("main-reveal-h2").style.opacity = "0";
-    document.getElementById("main-reveal-h2").style.fontSize = "0";
-    setTimeout(revealInfo, 250);
-    // document.getElementById("main-reveal-h1").style.width = "0";
-    // document.getElementById("main-reveal-h1").style.height = "0";
+    if (hidden == false) {
+        document.getElementById("main-reveal-h1").style.opacity = "0";
+        document.getElementById("main-reveal-h1").style.fontSize = "0";
+        document.getElementById("main-reveal-h2").style.opacity = "0";
+        document.getElementById("main-reveal-h2").style.fontSize = "0";
+        setTimeout(revealInfo, 250);
+        // document.getElementById("main-reveal-h1").style.width = "0";
+        // document.getElementById("main-reveal-h1").style.height = "0";
+    }
 }
 
-// Recursive Method
 function revealInfo() {
-
+    // document.getElementById("mainbody").style.width = "0";
+    //document.getElementById("mainbody").style.height = "0";
+    if (hidden == false) {
+        document.getElementById("mainbody").style.display = "none";
+        document.getElementById("main-one-info").style.marginLeft = "30vw";
+        document.getElementById("main-one-info").style.width = "40vw";
+        document.getElementById("main-one-info").style.backgroundColor = "#000000";
+        hidden = true;
+    }
 }
