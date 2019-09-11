225c9b4a3a0240cc964420ed701cdb4e3abf86b5
Cameronl14320 <41677875+Cameronl14320@users.noreply.github.com>
2019-09-11 23:24:57 +1200

    Core completed somewhat

--- docs/landing/script.js
+++ docs/landing/script.js
@@ -8,12 +8,19 @@ function hideReveal() {
         document.getElementById("reveal-info").style.height = "0";
         // document.getElementById("reveal-info").style.display = "none";
         setTimeout(hideMainBodyText, 125);
+    } else if (hidden == true) {
+        document.getElementById("reveal-info").style.opacity = "1";
+        document.getElementById("reveal-info").style.width = "5vw";
+        document.getElementById("reveal-info").style.height = "5vw";
+        hidden = false;
+        console.log("should be working here");
     }
 }
 
 function hideMainBodyText() {
     console.log("works hideMainBodyText");
     if (hidden == false) {
+        document.getElementById("reveal-info").style.display = "none";
         document.getElementById("main-reveal-h1").style.opacity = "0";
         document.getElementById("main-reveal-h1").style.fontSize = "0";
         document.getElementById("main-reveal-h2").style.opacity = "0";
@@ -21,6 +28,13 @@ function hideMainBodyText() {
         setTimeout(revealInfo, 250);
         // document.getElementById("main-reveal-h1").style.width = "0";
         // document.getElementById("main-reveal-h1").style.height = "0";
+    } else if (hidden == true) {
+        document.getElementById("reveal-info").style.display = "inline-block";
+        document.getElementById("main-reveal-h1").style.opacity = "1";
+        document.getElementById("main-reveal-h1").style.fontSize = "2vw";
+        document.getElementById("main-reveal-h2").style.opacity = "1";
+        document.getElementById("main-reveal-h2").style.fontSize = "2vw";
+        setTimeout(hideReveal, 250);
     }
 }
 
@@ -28,19 +42,55 @@ function revealInfo() {
     // document.getElementById("mainbody").style.width = "0";
     //document.getElementById("mainbody").style.height = "0";
     if (hidden == false) {
-        document.getElementById("mainbody").style.display = "none";
-        document.getElementById("main-one-info").style.marginLeft = "20vw";
-        document.getElementById("main-one-info").style.width = "60vw";
-        document.getElementById("main-one-info").style.backgroundColor = "#000000";
+        //document.getElementById("mainbody").style.display = "none";
+        document.getElementById("main-reveal-h1").style.display = "none";
+        document.getElementById("main-reveal-h2").style.display = "none";
+
+
+        
+        document.getElementById("mainbody").style.width= "60vw";
+        document.getElementById("mainbody").style.left = "20vw";
+        
+        document.getElementById("main-one-info").style.marginLeft = "0";
+        document.getElementById("main-one-info").style.width = "100%";
+        document.getElementById("main-one-info").style.backgroundColor = "#dfdfdf";
         setTimeout(profileInfo, 250);
+    } else if (hidden == true) {
+        document.getElementById("main-reveal-h1").style.display = "block";
+        document.getElementById("main-reveal-h2").style.display = "block";
+
+        document.getElementById("mainbody").style.width= "50vw";
+        document.getElementById("mainbody").style.left = "25vw";
+
+        document.getElementById("main-one-info").style.marginLeft = "90vw";
+        document.getElementById("main-one-info").style.width = "0";
+        document.getElementById("main-one-info").style.backgroundColor = "none";
+        setTimeout(hideMainBodyText, 1000);
     }
 }
 
 function profileInfo() {
     if (hidden == false) {
-        document.getElementById("info-portrait").style.width = "50%";
         document.getElementById("info-portrait").style.opacity = "1";
+        document.getElementById("info-description").style.opacity = "1";
+        document.getElementById("info-description").style.fontSize = "20px";
+        setTimeout(revealClose, 1200);
+    } else if (hidden == true) {
+        document.getElementById("info-portrait").style.opacity = "0";
+        document.getElementById("info-description").style.opacity = "0";
+        document.getElementById("info-description").style.fontSize = "0px";
+        setTimeout(revealInfo, 300);
 
-        hidden = true;
+    }
+}
+
+function revealClose() {
+    if (hidden == false) {
+    document.getElementById("info-close").style.opacity = "1";
+    document.getElementById("info-close").style.marginLeft = "0";
+    hidden = true;
+    } else if (hidden == true) {
+        document.getElementById("info-close").style.opacity = "0";
+        setTimeout(profileInfo, 150);
     }
 }
