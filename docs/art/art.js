674e72c5cc2efd988f4c3e166a7943ba3e51e17e
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-02-25 19:46:05 +1300

    fixed cv text and added img to art

--- docs/art/art.js
+++ docs/art/art.js
@@ -1,17 +1,24 @@
+var displaying = false;
+
 function upsize(newdisplay) {
     if (newdisplay != null) {
         document.getElementById('content').style.opacity = ".5";
         document.getElementById('revealimg').src = newdisplay;
         document.getElementById('revealdisplay').style.visibility = 'visible';
         document.getElementById('revealdisplay').style.opacity = '1';
+        document.getElementById('closeimg').style.visibility = 'visible';
+        displaying = true;
     } else {
         console.log("no image found for newdisplay: " + newdisplay)
     }
 }
 
 function downsize() {
-    document.getElementById('revealdisplay').style.opacity = '0';
-    document.getElementById('revealdisplay').style.visibility = 'hidden';
-    document.getElementById('content').style.opacity = "1";
-    console.log("invalid");
+    if (displaying == true) {
+        document.getElementById('closeimg').style.visibility = 'hidden';
+        document.getElementById('revealdisplay').style.opacity = '0';
+        document.getElementById('revealdisplay').style.visibility = 'hidden';
+        document.getElementById('content').style.opacity = "1";
+        displaying = false;
+    }
 }
